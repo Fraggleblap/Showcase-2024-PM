c1d939ddce42fd497e982a5a3ee2f0dc44df1119
Fraggleblap <Owen.Gaffney23@student.cart.org>
2023-12-06 14:13:38 -0800

    the

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
 const
     // set the name of your shop here
-    shopID = 'JPMerch5',
+    shopID = 'JPMerch6',
     // match the following attributes to the classes on your products
     productClass = 'product',
     imageClass = 'prodImage',
